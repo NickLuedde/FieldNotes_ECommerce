1fa6cfd8484f46314a8eed64b23d5e8f5d20b318
NICK LUEDDE <LUEDDENICHOLAS@GMAIL.COM>
2020-12-11 10:53:49 -0600

    Initial Solution Setup with Projects

---
+++ FieldNotes_ECommerce/FieldNotes_ECommerce.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FieldNotes_ECommerce.WebUI.MvcApplication" Language="C#" %>
---
+++ FieldNotes_ECommerce/FieldNotes_ECommerce.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FieldNotes_ECommerce.WebUI.Startup))]
+namespace FieldNotes_ECommerce.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
