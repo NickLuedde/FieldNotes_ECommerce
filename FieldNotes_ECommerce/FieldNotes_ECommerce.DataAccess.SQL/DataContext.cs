eaef32bafbfca226c50924e741df9b59b81f0f06
NICK LUEDDE <LUEDDENICHOLAS@GMAIL.COM>
2021-01-05 10:20:07 -0600

    Changes EF & SQL

---
+++ FieldNotes_ECommerce/FieldNotes_ECommerce.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using FieldNotes.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldNotes_ECommerce.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base ("DefaultConnection")
+        {
+      
+        }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
